80f59af0f05884f49bcc2c094edd614455089b0c
tomoe <593575498@qq.com>
2020-12-03 23:45:33 +0800

    练习

---
+++ test12_1/src/TestDemo.java
@@ -0,0 +1,14 @@
+public class TestDemo {
+
+//    public static void main(String[] args) {
+//        System.out.println("hello world!");
+//    }
+
+    int num = 10;
+    // 方法1
+    String str1 = num + "";
+    // 方法2
+    String str2 = String.valueOf(num);
+    String str = "100";
+    int num2= Integer.parseInt(str);
+}
