fbdcd8c9755be24a6f6bf0e782e42b9927998eab
tomoe <593575498@qq.com>
2020-12-05 17:54:16 +0800

    练习

---
+++ test12_5/src/test12_5.java
@@ -0,0 +1,34 @@
+import java.sql.SQLOutput;
+
+public class test12_5 {
+    public static void main(String args[]) {
+
+//        StringBuffer sb = new StringBuffer("我在学习Java语言，共");
+//        System.out.println("原字符串是："+sb);
+//        System.out.println("原字符串长度是："+sb.length());
+//        System.out.println("添加后的字符串是："+sb.append("学时。"));
+//        System.out.println("插入后的字符串是："+sb.insert(8,"程序设计"));
+//        System.out.println("插入数字后的字符串是："+sb.insert(16,60));
+//        System.out.println("替换后的字符串是："+sb.replace(0,1,"本学期"));
+//        System.out.println("删除后的字符串是："+sb.deleteCharAt(16));
+//        System.out.println("逆序排列后的字符串是："+sb.reverse());
+//        System.out.println("处理后字符串是："+sb);
+
+        String str="Hello Java, Hello Java String";
+        System.out.println("从索引位置2开始，第一个Hello的索引位置是："+str.indexOf("Hello",5));
+        System.out.println("从索引位置12开始，最后一个Java所在的索引位置是："+str.lastIndexOf("Java",10));
+
+
+//        String s1 = "Hello";
+//        String s2 = "Hello";
+//        String s3 = new String("Hello");
+//        String s4= "HELLO";
+//        String s5 = ",Good-bye";
+//        System.out.println("s1.equals(s2)："+s1.equals(s2));     //true
+//        System.out.println("s1==s2："+(s1==s2));                 //true
+//        System.out.println("s1==s3："+(s1==s3));                 //false
+//        System.out.println("s1.equals(s4)："+s1.equals(s4));     //true
+//        System.out.println("s1.equalsIgnoreCase(s4)："+s1.equalsIgnoreCase(s4));        //true
+//        System.out.println("s1.concat(s5)："+s1.concat(s5));
+}
+}
