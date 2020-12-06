ea064e9b030dba89d5a72b84206f922747e76ba4
tomoe <593575498@qq.com>
2020-12-06 17:32:03 +0800

    练习

---
+++ test12_6/src/Test12_6.java
@@ -0,0 +1,45 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Test12_6 {
+//    public static void main(String[] args) {
+//        Scanner scan=new Scanner(System.in);
+//
+//        String line=scan.nextLine();
+//        System.out.println("您输入的一行内容是："+line);
+//
+//        int number=scan.nextInt();
+//        System.out.println("您输入的数字是："+number);
+//
+//        int count=0;
+//        while(scan.hasNext()){
+//            if(count++==3){
+//                scan.close();
+//                System.exit(0);
+//            }else{
+//                String str=scan.next();
+//                System.out.println("您输入的第"+"个字符串是："+str);
+//            }
+//        }
+//    }
+//    public static void main(String[] args) {
+//        Random rm=new Random();
+//        System.out.println(rm.nextInt(100));       //随机从0~100之间选取一个随机数
+//        System.out.println(rm.nextInt());
+//        System.out.println(rm.nextBoolean());
+//        System.out.println(rm.nextDouble());
+//        System.out.println("****************************");
+//
+//        rm=new Random(47);                        //设置随机种子，当再次运行时，结果不变
+//        System.out.println(rm.nextInt(100));
+//        System.out.println(rm.nextInt());
+//        System.out.println(rm.nextInt());
+//}
+
+
+
+}
+
+
+
