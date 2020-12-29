1b82143e3c79857a0d14988b8c9df64fddca7846
tomoe <593575498@qq.com>
2020-12-29 19:34:11 +0800

    练习

---
+++ test12_29/MusicPlayer.java
@@ -0,0 +1,19 @@
+﻿import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javazoom.jl.decoder.JavaLayerException; 
+import javazoom.jl.player.Player;
+//播放mp3文件
+public class MusicPlayer {
+	
+	public static void main(String[] args) throws JavaLayerException, FileNotFoundException {
+		File file = new File("E:\\hchj69\\java\\musicbox\\already.mp3");
+		FileInputStream fis = new FileInputStream(file);
+		BufferedInputStream stream = new BufferedInputStream(fis);
+		Player player = new Player(stream);
+		player.play();
+	}
+	
+}
