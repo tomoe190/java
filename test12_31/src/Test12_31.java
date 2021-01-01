68e8c05d5b3c6218feb83a6994a3c86f41de7d7a
tomoe <593575498@qq.com>
2021-01-01 23:13:46 +0800

    练习

---
+++ test12_31/src/Test12_31.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Test12_31 {
+    public static void main(String[] args) {
+       //求两个正整数的最大公约数
+//        Random random=new Random();
+//        int toGuess=random.nextInt(100)+1;
+//        Scanner scanner=new Scanner(System.in);
+//        while(true){
+//            System.out.println("请输入猜测的数：");
+//            int num=scanner.nextInt();
+//            if(num<toGuess){
+//                System.out.println("猜小了。");
+//            }
+//            else if(num>toGuess){
+//                System.out.println("猜大了。");
+//            }
+//            else{
+//                System.out.println("你猜对了！");
+//                break;
+//            }
+//        }
+
+        //阶乘求和
+        int result=0;
+        for (int num = 1; num <=5; num++) {
+            int factorResult=1;
+            for (int j = 1; j <=num; j++) {
+                factorResult*=j;
+            }
+            result+=factorResult;
+        }
+        System.out.println(result);
+    }
+}
