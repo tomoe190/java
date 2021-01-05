048f3dca0bcb9b8f7f48b00e4aab0f2b7f3f1f4a
tomoe <593575498@qq.com>
2021-01-05 21:58:37 +0800

    add

---
+++ 2021/src/Test1_5.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Test1_5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+    }
+}
