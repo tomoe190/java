593228b0b1351061b58f8aba14946fc058f1ecee
tomoe <593575498@qq.com>
2021-01-07 23:25:47 +0800

    add

--- Test1_7.java
+++ Test1_7.java
@@ -1,8 +1,7 @@
-import java.sql.SQLOutput;
-import java.util.Scanner;
+import javax.imageio.stream.ImageInputStream;
 
 public class Test1_7 {
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
 //        int result = 0;
 //        for (int num = 1; num <= 5; num++) {
 //            int factorResult = 1;
@@ -39,12 +38,14 @@ public class Test1_7 {
 //        int minValue = min(a,b);
 //        System.out.println("最小值是："+minValue);
 //    }
-//    public static int min(int a, int b) {
-//        if(a>b){
-//            return b;
-//        }else{
-//            return a;
-//        }
+//    }
+
+    public static int min(int a, int b) {
+        if (a > b) {
+            return b;
+        }
+        return a;
+    }
 
 
         //打印1-100之间的所有素数
@@ -74,13 +75,90 @@ public class Test1_7 {
 //        return true;
 
 
-        //输出1000-2000之间所有的闰年
-        printLeapyear(1000,2000);
+//        //输出1000-2000之间所有的闰年
+//        printLeapYear(1000,2000);
+//    }
+//    public static void printLeapYear(int begin, int end) {
+//        for (int i = begin; i <= end; i++) {
+//            if(isLeapYear(i)){
+//                System.out.println(i);
+//            }
+//        }
+//    }
+//    public static boolean isLeapYear(int year) {
+//        if(year%100==0){
+//            if(year%400==0){
+//                return true;
+//            }
+//            return false;
+//        }else(year%4==0){
+//            return true;
+//        }
+//        return false;
 
+        //打印乘法口诀表
+//        printMultiplicationTable(9);
+//    }
+//    public static void printMultiplicationTable(int num) {
+//        for (int row = 1; row <= num; row++) {
+//            printLine(row);
+//        }
+//    }
+//    public static void printLine(int row) {
+//        for (int col = 1; col <=row; col++) {
+////            System.out.print(col+"X"+row+"="+row*col+"  ");
+//            System.out.printf("%dx%d=%-2d ",col,row,col*row);
+//        }//                           至少两位，如果是一位数就空一位
+//        System.out.println();
+
+
+        //求两个整数的最大公约数
+//        public static void main(String[] args) {
+//            int result = calcGCD(100,120);
+//            System.out.println(result);
+//        }
+//    public static int calcGCD(int a, int b) {
+//        int min=min(a,b);
+//        for (int i = min; i >0; i--) {
+//            if(isGCD(a,b,i)){
+//                return i;
+//            }
+//        }
+//        return 0;
+//    }
+//    public static boolean isGCD(int a, int b, int i) {
+//        if(a%i==0 && b%i==0){
+//            return true;
+//        }
+//        return false;
+//    }
+
+    //编写程序数一下 1到 100 的所有整数中出现多少个数字9
+    public static void main(String[] args) {
+        int count = calcNine(1,100);
+        System.out.println(count);
     }
-
-    public static int printLeapyear(int begin, int end) {
-
-        return 0;
+    public static int calcNine(int begin, int end) {
+        int count =0;
+        for (int i = begin; i <=end; i++) {
+            count +=result(i);
+        }
+        return count;
+    }
+    public static int result(int number) {
+        int count =0;
+//        if(number%10==9){
+//            count++;
+//        }
+//        if(number/10%10==9){
+//            count++;
+//        }
+        while (number >= 9) {
+            if(number%10==9){
+                count++;
+            }
+            number/=10;
+        }
+        return count;
     }
 }
