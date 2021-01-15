c831016dad7cc9bdc9af14e6ec91700401b4b0ec
tomoe <593575498@qq.com>
2021-01-15 23:44:05 +0800

    数组

---
+++ 2333/src/Test1_15.java
@@ -0,0 +1,33 @@
+public class Test1_15 {
+    public static void main(String[] args) {
+
+//        for (int i = 0; i < arr.length ; i++) {
+//            System.out.println(arr[i]);
+//        }
+//        for (int x : arr){
+//            System.out.println(x);
+//        }
+        int[] arr = {1,2,3,4};
+        int[] a = arr;
+        transform(a);
+        int[] result = transform2(a);
+        printArray(arr);
+        printArray(result);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+    public static void transform(int[] a){
+        a[0] = 100;
+    }
+    public static int[] transform2(int[] a){
+        int[] result = new int[a.length];
+        for (int i = 0; i <a.length ; i++) {
+            result[i] = a[i] * 2;
+        }
+        return result;
+    }
+}
