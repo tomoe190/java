d5fe7d5d453d7b2d8364029c79d58b33547e66c9
tomoe <593575498@qq.com>
2021-01-17 21:29:41 +0800

    数组练习

--- 2333/src/Test1_16.java
+++ 2333/src/Test1_16.java
@@ -18,9 +18,79 @@ public class Test1_16 {
 //        int searchResult = search(arr,7);
 //        System.out.println(searchResult);
 //        int[] a = {1,2,3,5,7,9,11,15,18,21};
-        int[] a = makeBigArray();
-        int pos = binarySearch(a,9999);
-        System.out.println(pos);
+//        int[] a = makeBigArray();
+//        int pos = binarySearch(a,9999);
+//        System.out.println(pos);
+
+
+
+        int[] arr = {9,5,2,7,3,6};
+//        bubbleSort(arr);
+//        System.out.println(arr);
+//        Arrays.sort(arr);
+//        reverse(arr);
+//        System.out.println(Arrays.toString(arr));
+        transform2(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void transform2(int[] arr) {
+        int left = 0;
+        int right = arr.length-1;
+        while(left<right){
+            //先从左往右找到一个奇数
+            while(left<right && arr[left] % 2 ==0){
+                left++;
+            }
+            //从右往左找到一个偶数
+            while(left<right && arr[right] % 2 !=0){
+                right--;
+            }
+            //交换left和right
+            int tmp =arr[left];
+            arr[left] = arr[right];
+            arr[right] = tmp;
+        }
+    }
+
+    public static void reverse(int[] arr){
+        int left = 0;
+        int right = arr.length-1;
+        while(left<right){
+            int tmp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = tmp;
+            left ++;
+            right --;
+        }
+    }
+
+    public static void bubbleSort(int[] arr) {
+        //从后往前遍历
+        //外层循环遍历的次数
+        //已排区间[0，bound）
+        //待排区间[bound，length）
+        for (int bound = 0; bound <arr.length ; bound++) {
+        //里层循环，比较交换
+            for (int cur = arr.length-1; cur >0 ; cur--) {
+                if(arr[cur - 1] > arr[cur]){
+                    //不符合排序要求
+                    int tmp = 0;
+                    tmp = arr[cur - 1];
+                    arr[cur - 1] = arr[cur];
+                    arr[cur] = tmp;
+                }
+            }
+        }
+    }
+
+    public static boolean isSorted(int[] arr) {
+        for (int i = 0; i <arr.length-1 ; i++) {
+            if(arr[i] > arr[i+1]){
+                return false;
+            }
+        }
+        return true;
     }
 
     public static int[] makeBigArray(){
@@ -37,7 +107,7 @@ public class Test1_16 {
         int right = a.length - 1;
         while (left <= right ) {
             count++;
-            int mid = left + right / 2;
+            int mid = (left + right) / 2;
             if( toSearch > a[mid] ){
                 left = mid + 1;
             }else if( toSearch < a[mid]){
---
+++ 2333/src/Test1_17.java
@@ -0,0 +1,176 @@
+import java.util.Arrays;
+
+public class Test1_17 {
+    public static void main(String[] args) {
+//        int[] arr = {1,1,2,2,5,5,12,12,8,4,4,6,6};
+//        int onlyOneNumber = onlyOne(arr);
+//        System.out.println(onlyOneNumber);
+
+//        int[] arr = {2,3,5,1,11,6,8};
+//        double avgValue = avg(arr);
+//        System.out.println(avgValue);
+//        int sumOfAll = sum(arr);
+//        System.out.println(sumOfAll);
+//        transform(arr);
+//        System.out.println(Arrays.toString(arr));
+//        printArray(arr);
+//        int[] arr = makeArray();
+//        System.out.println(Arrays.toString(arr));
+//        bubbleSort(arr);
+//        System.out.println(Arrays.toString(arr));
+
+//        int[] arr = {0,2,3,4,10,8,9};
+//        System.out.println(isIncrease(arr));
+
+        int[] arr = {2,3,6,10,11,18,21,44,56,81,99};
+//        int index = searchNumber(arr,11);
+//        System.out.println(index);
+//        int[] arr2 = copyOf(arr);
+//        System.out.println(Arrays.toString(arr2));
+
+        String arrToString = toString(arr);
+        System.out.println(arrToString);
+    }
+
+    private static String toString(int[] arr) {
+        String result = "[";
+        for (int i = 0; i < arr.length; i++) {
+            result += arr[i];
+            if (i != arr.length - 1) {
+                result += ",";
+            }
+        }
+        result += "]";
+        return result;
+    }
+
+    private static int[] copyOf(int[] arr) {
+        int[] newArray = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            newArray[i] = arr[i];
+        }
+        return newArray;
+    }
+
+    private static int searchNumber(int[] arr, int toSearch) {
+        int left = 0;
+        int right = arr.length - 1;
+        while(left <= right){
+            int mid = (left + right) / 2;
+            if(toSearch < arr[mid]){
+                right = mid -1;
+            }else if(toSearch > arr[mid]){
+                left = mid +1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    private static boolean isIncrease(int[] arr) {
+        for (int i = 0; i <arr.length - 1 ; i++) {
+            if(arr[i] > arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void bubbleSort(int[] arr) {
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = 0; j <arr.length - i - 1 ; j++) {
+                if(arr[j] > arr[j+1]){
+                    int tmp =arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = tmp;
+                }
+            }
+        }
+    }
+
+    private static int[] makeArray() {
+        int[] newArray = new int[100];
+        for (int i = 0; i < newArray.length; i++) {
+            newArray[i] = i + 1;
+            }
+        return newArray;
+    }
+
+    private static void printArray(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i <arr.length ; i++) {
+            if (i != arr.length - 1) {
+                System.out.print(arr[i] + ",");
+            }else{
+                System.out.print(arr[i]);
+            }
+        }
+        System.out.println("]");
+    }
+
+    private static void transform(int[] arr) {
+        for (int i = 0; i <arr.length ; i++) {
+            arr[i] = arr[i] * 2;
+        }
+    }
+
+    public static int sum(int[] arr) {
+        int sum = 0;
+        for (int value : arr) {
+            sum += value;
+        }
+        return sum;
+    }
+
+    public static double avg(int[] arr) {
+        int sum = 0;
+        for (int value : arr) {
+            sum += value;
+        }
+        return (double)sum / arr.length;
+    }
+
+
+    private static int onlyOne(int[] arr) {
+        int count = 0;
+        for (int i = 0; i <arr.length ; i++) {
+            for (int j = 0; j <arr.length ; j++) {
+                if(arr[i] == arr[j]){
+                    count ++;
+                }
+            }
+            if(count == 1){
+                return arr[i];
+            }
+            count = 0;
+        }
+        return -1;
+    }
+
+
+//    public static void main(String[] args) {
+//        int[] arr = {8,5,4,2,3,6,11,9};
+//        reverse(arr);
+//        System.out.println(Arrays.toString(arr));
+//    }
+//
+//    private static void reverse(int[] arr) {
+//        int left = 0;
+//        int right = arr.length - 1;
+//        while(left < right){
+//            while(left < right && arr[left] % 2 != 0){
+//                left ++;
+//            }
+//            while(left < right && arr[right] % 2 == 0){
+//                right --;
+//            }
+//
+//            if (left < right) {
+//                int tmp = arr[left];
+//                arr[left] = arr[right];
+//                arr[right] = tmp;
+//            }
+//        }
+//    }
+}
