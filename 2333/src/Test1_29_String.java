14fd25448cc195f4a9ea5a62c6b9c408f44fc21b
tomoe <593575498@qq.com>
2021-01-29 23:21:20 +0800

    String

---
+++ 2333/src/Test1_29_String.java
@@ -0,0 +1,26 @@
+import Test1_25.Cat;
+
+public class Test1_29_String {
+    public static void main(String[] args) {
+//        String a = "hello";
+//        String b = "hello";
+//        System.out.println(a == b);
+//        System.out.println(a.equals(b));
+//        String c = new String("hello");
+//        System.out.println(a == c);
+//        System.out.println(a.equals(c));
+
+//        String a = null;
+//        if(a.equals("hello")) {
+//        }
+//        if(("hello").equals(a)) {
+//
+//        }
+
+//        String a = "hello";
+//        String b = new String("hello").intern();
+//        System.out.println(a == b);
+
+        String a = "hello";
+    }
+}
