03c4f3db58c985d40df5fb92ec427698b1e960e4
tomoe <593575498@qq.com>
2021-02-02 19:40:16 +0800

    时间复杂度/空间复杂度

--- 2333/src/Test2_2.java
+++ 2333/src/Test2_2.java
@@ -1,13 +1,46 @@
+import java.util.Scanner;
+
 public class Test2_2 {
     public static void main(String[] args) {
 //        int[] arr = {1,3,5,8,9,10,12,131,410};
 //        int index = binarySearch(arr,10);
 //        System.out.println(index);
-        int num = fibonacci(5);
-        System.out.println(num);
+//        int num = fibonacci(6);
+//        System.out.println(num);
+//        int result = sum(12345);
+//        System.out.println(result);
+//        int n = 10;
+//        int result = sumNum(n);
+//        System.out.println(result);
+
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int result = factorial(num);
+        System.out.println(result);
     }
 
-//    public static void bubbleSort(int[] array) {
+    private static int factorial(int num) {
+        if(num == 1) {
+            return 1;
+        }
+        return num * factorial(num - 1);
+    }
+
+    private static int sumNum(int n) {
+        if(n == 1) {
+            return 1;
+        }
+        return n + sumNum(n - 1);
+    }
+
+//    private static int sum(int num) {
+//        if (num < 10) {
+//            return num;
+//        }
+//        return num%10 + sum(num / 10);
+//    }
+
+//    private static void bubbleSort(int[] array) {
 //        for (int end = array.length; end > 0; end--) {
 //            boolean sorted = true;
 //            for (int i = 1; i < end; i++) {
@@ -22,14 +55,14 @@ public class Test2_2 {
 //        }
 //    }
 
-    public static int fibonacci(int N) {
-        if (N <= 2) {
+    private static int fibonacci(int N) {
+        if(N <= 2) {
             return 1;
         }
         return fibonacci(N - 1) + fibonacci(N - 2);
     }
 
-    public static int binarySearch(int[] arr,int toSearch) {
+    private static int binarySearch(int[] arr,int toSearch) {
         int left = 0;
         int right = arr.length - 1;
         while(left <= right){
