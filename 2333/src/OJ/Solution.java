14f2041cd3d598e314b4358caf3f48ce0c977a03
tomoe <593575498@qq.com>
2021-02-06 23:24:04 +0800

    反转链表

--- 2333/src/OJ/Solution.java
+++ 2333/src/OJ/Solution.java
@@ -20,16 +20,49 @@ public class Solution {
         return head;
     }
 
+    public static ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return head;
+        }
+        ListNode prevNode = null;
+        ListNode curNode = head;
+        while (curNode != null) {
+            ListNode nextNode = curNode.next;
+            curNode.next = prevNode;
+            prevNode = curNode;
+            curNode = nextNode;
+        }
+        return prevNode;
+    }
+
     public static void main(String[] args) {
-        ListNode head = new ListNode(1);
-        int val = 0;
-        head = removeElements(head,val);
+//        ListNode head = new ListNode(1);
+//        int val = 1;
+//        head = removeElements(head,val);
+//        traversal(head);
+
+        ListNode a = new ListNode(1);
+        ListNode b = new ListNode(2);
+        ListNode c = new ListNode(3);
+        ListNode d = new ListNode(4);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = null;
+        ListNode head = a;
         traversal(head);
+        ListNode result = reverseList(head);
+        traversal(result);
+
     }
 
     private static void traversal(ListNode head) {
         for(ListNode cur = head;cur != null;cur = cur.next) {
-            System.out.println(cur.val);
+            System.out.print(cur.val + " ");
         }
+        System.out.println();
     }
 }
