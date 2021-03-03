dc06acd7971bb253221fba38059db7527ff1ce80
tomoe <593575498@qq.com>
2021-03-03 22:10:18 +0800

    homework_Day12

--- 2333/src/homework/Day12.java
+++ 2333/src/homework/Day12.java
@@ -1,7 +1,7 @@
 package homework;
 
 public class Day12 {
-    public int findUnsortedSubarray(int[] nums) {
+    public static int findUnsortedSubarray(int[] nums) {
         int len = nums.length;
         int max = nums[0];
         int min = nums[len - 1];
@@ -20,5 +20,38 @@ public class Day12 {
             }
         }
         return end - start + 1;
+    }
+
+    public static int evalRPN(String[] tokens) {
+        int[] numStack = new int[tokens.length / 2 + 1];
+        int index = 0;
+        for (String s : tokens) {
+            switch (s){
+                case "+" :
+                    numStack[index - 2] += numStack[-- index];
+                    break;
+                case "-" :
+                    numStack[index - 2] -= numStack[-- index];
+                    break;
+                case "/" :
+                    numStack[index - 2] /= numStack[-- index];
+                    break;
+                case "*" :
+                    numStack[index - 2] *= numStack[-- index];
+                    break;
+                default:
+                    numStack[index ++] = Integer.parseInt(s);
+                    break;
+            }
+        }
+        return numStack[0];
+    }
+
+    public static void main(String[] args) {
+//        String[] s ={"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"};
+//        System.out.println(evalRPN(s));
+
+        int[] nums = {2,6,4,8,10,9,15};
+        System.out.println(findUnsortedSubarray(nums));
     }
 }
