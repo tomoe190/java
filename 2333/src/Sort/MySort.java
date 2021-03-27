2401f87490fe92e17d3886e751cca6fb05148119
tomoe <593575498@qq.com>
2021-03-27 09:07:04 +0800

    排序

--- 2333/src/Sort/MySort.java
+++ 2333/src/Sort/MySort.java
@@ -1,6 +1,7 @@
 package Sort;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class MySort {
     public static void insertSort(int[] arr) {
@@ -99,12 +100,64 @@ public class MySort {
         }
     }
 
+    public static void bubbleSort(int[] arr) {
+        for (int bound = 0; bound < arr.length; bound++) {
+            for (int cur = arr.length - 1; cur > bound ; cur--) {
+                if (arr[cur - 1] > arr[cur]) {
+                    swap(arr,cur - 1,cur);
+                }
+            }
+        }
+    }
+
+    public static void quickSort(int[] arr) {
+
+        _quickSort(arr,0,arr.length - 1);
+    }
+
+    public static void _quickSort(int[] arr,int left,int right) {
+        if (left >= right) {
+            return;
+        }
+        // index 表示基准值所在的位置
+        int index = partition(arr,left,right);
+        _quickSort(arr,left,index - 1);
+        _quickSort(arr,index + 1,right);
+    }
+
+    public static int partition(int[] arr,int left,int right) {
+        int v = arr[right];
+        int l = left;
+        int r = right;
+        while (l < r) {
+            while (l < r && arr[l] <= v) {
+                l ++;
+            }
+            while (l < r && arr[r] >= v) {
+                r --;
+            }
+            swap(arr,l,r);
+        }
+        swap(arr,l,right);
+        return l;
+    }
+
+    public static void quickSortByLoop(int[] arr) {
+        // 1、创建一个栈用来保存当前的每一个待处理区间
+        Stack<Integer> stack = new Stack<>();
+        // 2、把根节点入栈，整个数组对应的区间
+        stack.push(0);
+        stack.push(arr.length - 1);
+    }
+
     public static void main(String[] args) {
         int[] arr = {8,7,2,5,6,9,1};
 //        insertSort(arr);
 //        shellSort(arr);
 //        selectOrder(arr);
-        heapSort(arr);
+//        heapSort(arr);
+//        bubbleSort(arr);
+        quickSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
