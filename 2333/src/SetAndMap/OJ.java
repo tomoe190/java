0eaeb669466baffe06cf1eea861ee739aec915d5
tomoe <593575498@qq.com>
2021-04-01 21:30:02 +0800

    OJ

--- 2333/src/SetAndMap/OJ.java
+++ 2333/src/SetAndMap/OJ.java
@@ -58,6 +58,44 @@ public class OJ {
         return count;
     }
 
+    public List<String> topKFrequent (String[] words,int k) {
+        // 1、使用 Map 统计 words 中每个单词出现的次数
+        Map<String,Integer> map = new HashMap<>();
+        for(String x : words) {
+//            Integer value = map.getOrDefault(x,0);
+//            map.put(x,value + 1);
+            Integer value = map.get(x);
+            if (value == null) {
+                map.put(x, 1);
+            } else {
+                map.put(x, value + 1);
+            }
+        }
+        // 2、把所有的 key 都取出来，放到一个 List 中
+        List<String> wordList = new ArrayList<>(map.keySet());
+        /**
+         * 3、把 wordList 进行排序，在 sort 的第二个参数中，
+         *   传入一个 Comparator 比较器对象，用来指定比较的规则。
+         *   这里创建了一个匿名内部类，并且创建了该类的实例，
+         *   这个内部类的类名不清楚，但知道这个类实现了 Comparator 接口，
+         *   { }中的内容就是匿名内部类的实现
+         */
+        // lambda 表达式相当于一个匿名的方法
+//        Collections.sort(wordList, (o1, o2) -> 0);
+        Collections.sort(wordList, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                Integer count1 = map.get(o1);
+                Integer count2 = map.get(o2);
+                if (count1.equals(count2)) {
+                    return o1.compareTo(o2);
+                }
+                return count2 - count1;
+            }
+        });
+        return wordList.subList(0,k);
+    }
+
     public static void main(String[] args) {
 //        int[] nums = {1,2,1,2,4};
 //        OJ oj = new OJ();
