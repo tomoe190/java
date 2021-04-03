3d14643563f57528f4f050b6b1608ca6ba2f6876
tomoe <593575498@qq.com>
2021-04-03 22:29:45 +0800

    setAndMap

---
+++ 2333/src/SetAndMap/BinarySearchTree.java
@@ -0,0 +1,17 @@
+package SetAndMap;
+
+class BinarySearchNode {
+    public int key;
+    public int value;
+    public BinarySearchNode left;
+    public BinarySearchNode right;
+
+    public BinarySearchNode(int key,int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+public class BinarySearchTree {
+    
+}
