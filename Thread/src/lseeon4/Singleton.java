4185de4e8aab11b47ea9244ee4b57382f18d42a1
tomoe <593575498@qq.com>
2021-06-11 23:02:33 +0800

    Thread

--- Thread/src/lseeon4/Singleton.java
+++ Thread/src/lseeon4/Singleton.java
@@ -6,9 +6,9 @@ public class Singleton {
     public static Singleton getInstance() {
         if (instance == null) {
             synchronized (Singleton.class) {
-//                if (instance == null) {
+                if (instance == null) {
                     instance = new Singleton();
-//                }
+                }
             }
         }
         return instance;
