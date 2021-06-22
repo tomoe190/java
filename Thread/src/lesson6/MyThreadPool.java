223670a34afadd4545e6f62a8d3411fb509ffc8f
tomoe <593575498@qq.com>
2021-06-22 15:35:06 +0800

    Thread

---
+++ Thread/src/lesson6/MyThreadPool.java
@@ -0,0 +1,52 @@
+package lesson6;
+
+import lesson5.MyBlockingQueue;
+
+public class MyThreadPool {
+
+    private MyBlockingQueue<Runnable> queue = new MyBlockingQueue<>(100);
+
+    // 传入的核心线程数来创建工作线程，创建线程池就启动
+    public MyThreadPool(int coreSize){
+        for (int i = 0; i < coreSize; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        while (true) {
+                            Runnable task = queue.take();//从仓库取任务
+                            task.run();
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+    }
+
+    public void execute(Runnable task) {
+        try {
+            queue.put(task);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThreadPool pool = new MyThreadPool(4);
+        for (int i = 0; i < 10; i++) {
+            pool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        System.out.println(1);
+                        Thread.sleep(99999);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ Thread/src/lesson6/SafeAdd.java
@@ -0,0 +1,23 @@
+package lesson6;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class SafeAdd {
+    
+    public static AtomicInteger COUNT = new AtomicInteger();
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0;i < 1000; i++) {
+                        COUNT.incrementAndGet();
+                    }
+                }
+            }).start();
+        }
+        while(Thread.activeCount()>1) Thread.yield();
+        System.out.println(COUNT.get());
+    }
+}
