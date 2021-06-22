53a167e76f42a14245aede91b4ea8dd833c48960
tomoe <593575498@qq.com>
2021-06-22 23:31:31 +0800

    Thread

--- Thread/src/lesson6/SafeAdd.java
+++ Thread/src/lesson6/SafeAdd.java
@@ -16,8 +16,6 @@ public class SafeAdd {
                     }
                 }
             }).start();
-        }
-        while(Thread.activeCount()>1) Thread.yield();
-        System.out.println(COUNT.get());
-    }
+   
+            
 }
