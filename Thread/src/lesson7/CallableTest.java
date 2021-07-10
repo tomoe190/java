e06ffa0acc8d7d38b6d5fa746c5e1c9ab8ac0b1d
tomoe <593575498@qq.com>
2021-07-10 18:26:23 +0800

    Thread

--- Thread/src/lesson7/CallableTest.java
+++ Thread/src/lesson7/CallableTest.java
@@ -1,10 +1,9 @@
 package lesson7;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 public class CallableTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         //Callable为任务描述，提供了任务执行完毕的返回结果
         Callable<Integer> c = new Callable<Integer>() {
             @Override
@@ -18,6 +17,9 @@ public class CallableTest {
 
         new Thread(task).start();
         System.out.println(3);
+        //Future/FutureTask.get()：当前线程阻塞等待，直到子线程执行完毕，返回结果
+        System.out.println(task.get());
+
 
 //        new Thread(new Runnable() {
 //            @Override
@@ -26,5 +28,32 @@ public class CallableTest {
 //            }
 //        }).start();
 //        System.out.println(2);
+
+
+        ExecutorService pool = Executors.newCachedThreadPool();
+
+        pool.execute(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+        pool.submit(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+        Future<String> future = pool.submit(new Callable<String>() {
+            @Override
+            public String call() throws Exception {
+                System.out.println(1);
+                return "2";
+            }
+        });
+        future.get();
+
+
+
     }
 }
